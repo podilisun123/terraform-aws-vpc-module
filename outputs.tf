4de5e2007fd27b994d716a3379da15f09fe689f6
sundar <sudar@gmail.com>
2025-05-04 14:54:55 +0530

    adding output variable

--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,6 @@ output "private_subnet_ids" {
 output "database_subnet_ids" {
     value = aws_subnet.database[*].id
 }
+output "database_subnet_group_name" {
+    value = aws_db_subnet_group.database_group.name
+}
